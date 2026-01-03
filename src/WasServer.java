3e755b600a9090661d1887c8bc02c3b0cd7ccaff
chc216 <mine3386@naver.com>
2026-01-03 14:33:35 +0900

    feat: Implement basic socket connection

--- src/WasServer.java
+++ src/WasServer.java
@@ -1,8 +1,28 @@
 //목표: 프로그램을 실행하고 브라우저로 localhost:8080에 접속했을 때, 프로그램이 꺼지지 않고 뭔가 반응이 있게 하기.
+
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.io.*;
 
 public class WasServer {
     public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(8080);
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String head = br.readLine();
+                if (head != null) {
+                    System.out.println(head);
+                }
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
