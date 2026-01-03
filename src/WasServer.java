f99a8719d37d6cc43fcfcddc77bf4bef96088dde
chc216 <mine3386@naver.com>
2026-01-03 19:24:57 +0900

    feat: Add Http logging, http response

--- src/WasServer.java
+++ src/WasServer.java
@@ -5,24 +5,42 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class WasServer {
     public static void main(String[] args) {
         try {
             ServerSocket serverSocket = new ServerSocket(8080);
-
             while (true) {
                 Socket socket = serverSocket.accept();
-                BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                String head = br.readLine();
-                if (head != null) {
-                    System.out.println(head);
+                System.out.println(socket.getPort());
+                System.out.println(socket.getInetAddress());
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                String line;
+                while((line = bufferedReader.readLine()) != null && !line.isEmpty()){
+                    System.out.println(line);
                 }
+
+                if(line == null) {
+                    socket.close();
+                    continue;
+                }
+
+                OutputStream out = socket.getOutputStream();
+                DataOutputStream dataOutputStream = new DataOutputStream(out);
+                byte[] body = "hello".getBytes();
+                dataOutputStream.writeBytes("HTTP/1.1 200 OK\n");
+                dataOutputStream.writeBytes("Content-type:text/html\n");
+                dataOutputStream.writeBytes("Content-length:" + body.length + "\n\n");
+                dataOutputStream.write(body, 0, body.length);
+                dataOutputStream.flush();
                 socket.close();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
 
     }
 }
