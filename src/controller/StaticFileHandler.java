4766d89af7242b8338f3d658cf8e2edc5a4bed2f
chc216 <mine3386@naver.com>
2026-02-27 17:43:02 +0900

    feat: 정적 파일 서빙 기능(StaticFileHandler) 구현 및 테스트 에셋 추가
    - MIME Type(HTML, 이미지 등)에 따른 Content-Type 동적 매핑 구현
     - TODO: 다형성을 이용한 동적 처리 구현 예정
    - Directory Traversal 공격 차단
    - Optional을 활용하여 파일 미존재 시 400(Bad Request) 예외 처리
    - JAR 배포 환경을 고려하여 클래스 로더 기반의 정적 자원 읽기 방식 채택
    - 테스트를 위한 정적 파일(HTML, 이미지) 에셋 추가

---
+++ src/controller/StaticFileHandler.java
@@ -0,0 +1,58 @@
+package controller;
+
+import http.HttpRequest;
+import http.HttpResponse;
+import http.StatusCode;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Optional;
+
+public class StaticFileHandler implements Controller{
+    final private String root = "/staticfile";
+    @Override
+    public void service(HttpRequest request, HttpResponse response) {
+        String requestUrl = request.getUrl();
+        String contentType = getContentType(requestUrl);
+        System.out.println("requestUrl: " + requestUrl);
+
+        if(requestUrl.contains("..")) {
+            //보안 위험으로 400 (요청 url을 ../../../한다면 root폴더에 접근이 가능해짐)
+            response.setStatus(StatusCode.BAD_REQUEST);
+            return;
+        }
+        Optional<byte[]> bytes = readFile(root + requestUrl);
+        if(bytes.isPresent()){
+            response.setByteBody(bytes.get());
+            response.setStatus(StatusCode.OK);
+            response.setContentType(contentType, "UTF-8");
+        } else {
+            //없으면 400으로 응답
+            System.out.println("bad url = " + requestUrl);
+            response.setStatus(StatusCode.BAD_REQUEST);
+        }
+    }
+
+
+    private String getContentType(String url) {
+        if (url.endsWith(".html")) return "text/html";
+        if (url.endsWith(".png")) return "image/png";
+        if (url.endsWith(".jpg")) return "image/jpeg";
+        return "text/plain";
+    }
+
+    //jar파일로 배포하기 때문에 클래스 로더를 이용하여 파일의 스트림을 가져오고 바이트 배열(옵셔널)을 반환한다.
+    private Optional<byte[]> readFile(String url) {
+        try (InputStream in = this.getClass().getResourceAsStream(url)) {
+            if (in == null) {
+                return Optional.empty();
+            }
+            return Optional.of(in.readAllBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Optional.empty();
+            //에러 처리 후에도 return을 해야함
+        }
+    }
+
+
+}
