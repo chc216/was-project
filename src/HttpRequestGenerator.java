b5301e33cf663cbb1a1c6f3dfb3902cc41feb0b9
chc216 <mine3386@naver.com>
2026-02-27 18:29:25 +0900

    chore: 향후 기능 추가를 위한 stub코드 추가  - HttpRequestGenerator: 지금은 Request객체 생성의 책임을 HttpParserUtils가 맡고 있으나 책임을 분리하여 해당 클래스에게 위임  - UnsupportedContentTypeException: 예외처리를 위한 커스텀 예외 클래스

---
+++ src/HttpRequestGenerator.java
@@ -0,0 +1,6 @@
+public class HttpRequestGenerator {
+//    public HttpRequest generate(InputStream in) throws IOException {
+//        return
+//    }
+    //아직은 필요없을거 같긴함 -> request생성 흐름 제어의 책임을 parserutils가 맡고 있기 때문임
+}
